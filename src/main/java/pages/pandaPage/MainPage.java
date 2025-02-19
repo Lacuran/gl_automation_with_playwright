243a76529f5f8d497f56d8d14d0a304ddc15b5e8
Lacuran <kornel.wrzesien@gmail.com>
2025-02-19 22:01:42 +0100

    Added another implementation for locators

--- src/main/java/pages/pandaPage/MainPage.java
+++ src/main/java/pages/pandaPage/MainPage.java
@@ -5,17 +5,23 @@ import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
 import io.github.uchagani.stagehand.annotations.Find;
 import io.github.uchagani.stagehand.annotations.PageObject;
+import io.github.uchagani.stagehand.annotations.Under;
 
 @PageObject
 public class MainPage extends BasePage {
   @Find("//*[text()='Mobile']")
   Locator mobileLink;
+
   @Find("//*[text()='TV']")
   Locator tvXpath;
+
   @Find("[data-target-element='#header-account']")
   Locator accountCssSelector;
+
+  @Under("accountCssSelector")
   @Find("[title='My Account']")
   Locator myAccountLink;
+
   @Find("[title='Advanced Search']")
   Locator advanceSearchLink;
 
--- src/test/java/login/LoginTest.java
+++ src/test/java/login/LoginTest.java
@@ -1,6 +1,7 @@
 package login;
 
 import base.test.BaseTestSetup;
+import io.github.uchagani.stagehand.PageFactory;
 import org.junit.jupiter.api.Test;
 import pages.pandaPage.MainPage;
 import pages.pandaPage.MyAccountPage;
@@ -15,7 +16,7 @@ public class LoginTest extends BaseTestSetup {
   @Test
   public void verifyIfUserIsLoggedSuccessfully() {
     final String expectedWelcomeMsg = "Hello, Kornel Maybe Test!";
-    MainPage mainPage = new MainPage(getPage());
+    MainPage mainPage = PageFactory.create(MainPage.class, getPage());
     MyAccountPage myAccountPage = mainPage.clickMyAccountLink().fillingLoginData(user1).clickLoginButton();
 
     assertEquals(expectedWelcomeMsg, myAccountPage.getWelcomeMsg());
