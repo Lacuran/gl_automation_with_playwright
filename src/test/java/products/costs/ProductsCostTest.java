e969b486f749a33321dc4f7af08b6ed908e355d3
Lacuran <kornel.wrzesien@gmail.com>
2025-04-16 17:31:34 +0200

    added products test to implement

---
+++ src/test/java/products/costs/ProductsCostTest.java
@@ -0,0 +1,37 @@
+package products.costs;
+
+import base.test.BaseTestSetup;
+import org.junit.jupiter.api.Test;
+
+public class ProductsCostTest extends BaseTestSetup {
+
+
+  /*@DataProvider(name = "smartphone")
+  public Object[][] dpMethod() {
+    return new Object[][]{
+        {"Sony Xperia"},
+        {"IPhone"},
+        {"Samsung Galaxy"}
+    };
+  }
+
+  @Test(dataProvider = "smartphone", description = "2")
+  public void verifyCostOfProductInListPageAndDetailsPageTest(String phoneName) {
+    PandaHomePage homePage = new PandaHomePage(driver);
+
+    log.info("2. Click on the 'MOBILE' menu");
+    ProductListingPage productListingPage = homePage.clickMobileLink();
+
+    log.info("3. In the list of all mobile, read the cost of Sony XPeria mobile. Note this value.");
+    String phoneCostOnMobilePage = productListingPage.getPhoneCostString(phoneName);
+
+    log.info("4. Click on the Sony XPeria mobile");
+    ProductDetailPage detailPage = productListingPage.clickPhoneCssSelector(phoneName);
+
+    log.info("5. Read the cost of mobile in detail page");
+    String phoneCostOnMobileDetailsPage = detailPage.getProductCost();
+
+    log.info("6. Compare value in Step 3 and Step 5");
+    assertEquals(phoneCostOnMobilePage, phoneCostOnMobileDetailsPage, "Compare value in Step 3 and Step 5");
+  }*/
+}
