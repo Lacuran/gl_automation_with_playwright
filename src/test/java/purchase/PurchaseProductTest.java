0ff80002d5bf4df339a6eab23f34ba67ba6aebd5
Lacuran <kornel.wrzesien@gmail.com>
2025-07-02 14:53:40 +0200

    Created class and added comment whats needs to be done in ths test case

---
+++ src/test/java/purchase/PurchaseProductTest.java
@@ -0,0 +1,71 @@
+package purchase;
+
+import base.test.BaseTestSetup;
+
+public class PurchaseProductTest extends BaseTestSetup {
+
+}
+
+
+
+/*
+* User user = User.createUserFrom("Userdata1.json").setEmail("lenrok.test1@gmail.com");
+
+    @BeforeGroups("purchase")
+    public void beforeTestSetUp() {
+
+        setUp();
+        PandaHomePage homePage = new PandaHomePage(driver);
+        homePage.clickMyAccountLink()
+                .fillingLoginData(user)
+                .clickLoginButton()
+                .clickTVLink()
+                .addToWishlist()
+                .openCart()
+                .emptyShoppingCart();
+        cleanUp();
+
+    }
+
+    @Test(groups = "purchase", description = "6")
+    public void verifyUserIsAbleToPurchaseProductUsingRegisteredEmailTest() {
+
+        PandaHomePage homePage = new PandaHomePage(driver);
+        Faker faker = new Faker();
+
+        String assertionShippingPriceMSG = "Check shipping price";
+        String expectedPrice = "$5.00";
+        String expectedTotalPrice = "$620.00";
+        String assertionTotalPriceMSG = "Check total price";
+        String newAddress = "New Address";
+        String streetAddress = faker.address().streetAddress();
+        String city = faker.address().cityName();
+        String zipcode = faker.address().zipCode();
+        String telephone = faker.phoneNumber().phoneNumber();
+        String[] shippingData = {user.getFirstName(), user.getMiddleName()
+                , user.getLastName(), streetAddress, city, zipcode, telephone};
+        String expectedOrderMSG = "YOUR ORDER HAS BEEN RECEIVED.";
+        String pageOrderAssertionErrorMSG = "Check order msg";
+
+        homePage.clickMyAccountLink()
+                .fillingLoginData(user)
+                .clickLoginButton()
+                .clickMyWishListLink()
+                .addToCartButton()
+                .fillShippingAndTaxField("PL", "Malopolska", zipcode)
+                .clickEstimateLink()
+                .assertShippingCost(expectedPrice, assertionShippingPriceMSG)
+                .selectRadioButton()
+                .clickUpdateTotalLink()
+                .assertTotalShippingCost(expectedTotalPrice, assertionTotalPriceMSG)
+                .clickCheckoutButton()
+                .selectNewBillingAddress(newAddress)
+                .fillNewBillingInformation(shippingData, "PL")
+                .clickBillingInformationContinueButton()
+                .clickShippingInformationContinueButton()
+                .selectCheckMoneyRadioButton()
+                .clickPaymentInformationContinueButton()
+                .clickPlaceOrderButton()
+                .assertOrderIsGenerated(expectedOrderMSG, pageOrderAssertionErrorMSG);
+    }
+* */
