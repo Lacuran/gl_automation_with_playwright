b3e06c0623ac9bc63d557ce8427845ebc5d5078c
Lacuran <kornel.wrzesien@gmail.com>
2025-07-02 14:48:15 +0200

    removed comments

--- src/test/java/shoppingcart/ShoppingCartTest.java
+++ src/test/java/shoppingcart/ShoppingCartTest.java
@@ -47,24 +47,4 @@ public class ShoppingCartTest extends BaseTestSetup {
     getBasePageFactory().getMobileDeviceListingPage().clickSecurityButton();
   }
 
-
-
-
-  /*
-  @Test(description = "3")
-    public void verifyThatYouCannotAddMoreProductInCartThanTheProductAvailableInTheStore() {
-        //variables
-        PandaHomePage homePage = new PandaHomePage(driver);
-        String expectedErrorMsg = "Some of the products cannot be ordered in requested quantity.";
-        String expectedEmptyCartMsg = "SHOPPING CART IS EMPT";
-
-        homePage.clickMobileLink()
-                .addToCart()
-                .changeQtyAndClickUpdate("1000")
-                .assertErrorMSG(expectedErrorMsg, "Verify error message")
-                .emptyShoppingCart()
-                .verifyTheCartIsEmpty(expectedEmptyCartMsg, "Verify msg for empty cart");
-
-    }
-    */
 }
